836890e4c98b0ae5958f8e61192b941e218930dd
Oluwaseye Owoborode <stevenowoborode@gmail.com>
2022-04-21 21:06:02 +0100

    Title: Confirmation of code (#5)
    * Title: Confirmation of code
    
    Body: This is an attempt to write the endpoint.
              Tests have been written to cover the username generator.
              Remove werant build
    
     Changes to be committed:
    	modified:   controller/base.go
            modified:   controller/base_test.go
    	modified:   server/server.go
            modified:   .gitignore

---
+++ controller/base_test.go
@@ -0,0 +1,48 @@
+package controller
+
+import (
+	"testing"
+)
+
+func TestUsernameGenratorLengthofCharacters(t *testing.T) {
+	email := "qwertydgfhrjs@gmail.com"
+	password := "password"
+	username := UserNameGenerator(email, password)
+	actualLength := len(username)
+	expectedLength := 28
+	if actualLength != expectedLength {
+		t.Errorf("Expected length of username to be %d, but got %d", expectedLength, actualLength)
+	}
+
+}
+
+func TestUsernameGenratornotGeneratingEmptyCharacters(t *testing.T) {
+	email := "qwertydgfhrjs@gmail.com"
+	password := "password"
+	username := UserNameGenerator(email, password)
+	expectedOutput := ""
+
+	if username == expectedOutput {
+		t.Errorf("Expected output to be %s, but got %s", expectedOutput, username)
+	}
+}
+
+func TestUsernameGenratornotgeneratingSameValueTwice(t *testing.T) {
+	email := "qwertydgfhrjs@gmail.com"
+	password := "password"
+	username := UserNameGenerator(email, password)
+	expectedOutput := UserNameGenerator(email, password)
+
+	if username == expectedOutput {
+		t.Errorf("Expected output to be %s, but got %s", expectedOutput, username)
+	}
+}
+
+func TestPasswordHshin(t *testing.T) {
+	password := "password"
+	hash, _ := HashPassword(password)
+	passwordMatch := CheckPasswordHash(password, hash)
+	if !passwordMatch {
+		t.Errorf("Expected password to match")
+	}
+}
